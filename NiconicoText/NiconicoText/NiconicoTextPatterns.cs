939b02913838472dbbbad42ff093a910b9a4ea6a
クワーティ <riot313@gmail.com>
2014-01-08 00:49:29 +0900

    created NiconicoTextPatternGenerator partial

--- NiconicoText/NiconicoText/NiconicoTextPatterns.cs
+++ NiconicoText/NiconicoText/NiconicoTextPatterns.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace NiconicoText
 {
-    class NiconicoTextPatterns
+    public static class NiconicoTextPatterns
     {
+        #region Public
+
+        public static string VideoIdPattern
+        {
+            get
+            {
+                return videoIdPattern;
+            }
+        }
+
+        #endregion
+
+
+        #region captures
+
+        internal const string channelIdCapture = nameStart + "channelId" + nameEnd + channelIdPattern + parenthesisEnd;
+        
+        internal const string communityIdCapture = nameStart + "communityId" + nameEnd + communityIdPattern + parenthesisEnd;
+
+        
+
+
+
+        
+
+        #endregion
+
+        #region patterns
+
+        private const string communityIdPattern = "\\bco\\d{1,14}\\b";
+
+        private const string channelIdPattern = "\\bch\\d{1,14}\\b";
+
+        private const string videoIdPattern = "\\b(?:sm|nm|so|ca|ax|yo|nl|ig|na|cw|z[a-e]|om|sk|yk)\\d{1,14}\\b";
+
+        #endregion
+
+        #region partial
+
+        private const string nameStart = "(?<";
+
+        private const string nameEnd = ">";
+
+        private const string parenthesisEnd = ")";
+
+        #endregion
     }
 }
