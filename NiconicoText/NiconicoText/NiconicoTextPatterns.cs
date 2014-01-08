5c5ecbb02bfbbc948640779e5f00488742ffd3ba
クワーティ <riot313@gmail.com>
2014-01-08 19:52:59 +0900

    modified regex pattern

---
+++ NiconicoText/NiconicoText/NiconicoTextPatterns.cs
@@ -0,0 +1,55 @@
+﻿
+
+namespace NiconicoText
+{
+    public static partial class NiconicoTextPatterns
+    {
+        			
+
+
+
+		
+		
+		public string CommunityIdPattern 
+		{
+			get
+			{
+				return communityIdPattern;
+			}
+		}
+
+		private const string communityIdPattern = @"co\d{1,14}" ;
+
+		internal const string communityIdGroupPattern = "(?<communityId>" +  communityIdPattern + ")";
+
+		
+		public string ChannelIdPattern 
+		{
+			get
+			{
+				return channelIdPattern;
+			}
+		}
+
+		private const string channelIdPattern = @"ch\d{1,14}" ;
+
+		internal const string channelIdGroupPattern = "(?<channelId>" +  channelIdPattern + ")";
+
+		
+		public string VideoIdPattern 
+		{
+			get
+			{
+				return videoIdPattern;
+			}
+		}
+
+		private const string videoIdPattern = @"(?:sm|nm|so|ca|ax|yo|nl|ig|na|cw|z[a-e]|om|sk|yk)\d{1,14}" ;
+
+		internal const string videoIdGroupPattern = "(?<videoId>" +  videoIdPattern + ")";
+
+		
+
+
+    }
+}
--- NiconicoText/NiconicoText/NiconicoTextPatterns.partial.cs
+++ NiconicoText/NiconicoText/NiconicoTextPatterns.partial.cs
@@ -8,51 +8,7 @@ namespace NiconicoText
 {
     public static partial class NiconicoTextPatterns
     {
-        #region Public
-
-        public static string VideoIdPattern
-        {
-            get
-            {
-                return videoIdPattern;
-            }
-        }
-
-        #endregion
 
 
-        #region captures
-
-        internal const string channelIdCapture = nameStart + "channelId" + nameEnd + channelIdPattern + parenthesisEnd;
-        
-        internal const string communityIdCapture = nameStart + "communityId" + nameEnd + communityIdPattern + parenthesisEnd;
-
-        
-
-
-
-        
-
-        #endregion
-
-        #region patterns
-
-        private const string communityIdPattern = "\\bco\\d{1,14}\\b";
-
-        private const string channelIdPattern = "\\bch\\d{1,14}\\b";
-
-        private const string videoIdPattern = "\\b(?:sm|nm|so|ca|ax|yo|nl|ig|na|cw|z[a-e]|om|sk|yk)\\d{1,14}\\b";
-
-        #endregion
-
-        #region partial
-
-        private const string nameStart = "(?<";
-
-        private const string nameEnd = ">";
-
-        private const string parenthesisEnd = ")";
-
-        #endregion
     }
 }
