cfcb2cb235f1ccfdf420b94f68a6b32f70856dd6
クワーティ <riot313@gmail.com>
2014-01-11 12:15:32 +0900

    created ArticleIdRegexText

--- NiconicoText/NiconicoTextTest/Tests/ChannelIdRegexTest.cs
+++ NiconicoText/NiconicoTextTest/Tests/ChannelIdRegexTest.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using NiconicoText;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace NiconicoTextTest.Tests
 {
-    class ChannelIdRegexTest
+    [TestClass]
+    public class ChannelIdRegexTest
     {
+        [TestMethod]
+        public void CreateTest()
+        {
+            var regex = createRegex();
+        }
+
+        private Regex createRegex()
+        {
+            return new Regex(NiconicoTextPatterns.channelIdGroupPattern);
+        }
     }
 }
