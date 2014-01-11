0a8427846976610f972def665227d5c99ec8eeca
クワーティ <riot313@gmail.com>
2014-01-11 10:28:25 +0900

    added videoId test

--- NiconicoText/NiconicoTextTest/Tests/VideoIdRegexTest.cs
+++ NiconicoText/NiconicoTextTest/Tests/VideoIdRegexTest.cs
@@ -14,7 +14,44 @@ namespace NiconicoTextTest.Tests
         [TestMethod]
         public void CreateTest()
         {
-            var videoIdRegex = new Regex(NiconicoTextPatterns.videoIdGroupPattern);
+            var videoIdRegex = createRegex();
+        }
+
+
+        [DataTestMethod]
+        [DataRow("http://www.nicovideo.jp/watch/sm22635959", "sm22635959",true)]
+        [DataRow("http://www.nicovideo.jp/watch/nm22635959", "nm22635959", true)]
+        [DataRow("http://www.nicovideo.jp/watch/nm22635959", "nm22635959", true)]
+        public void MatchTest(string text, string value, bool succeed)
+        {
+            var videoIdRegex = createRegex();
+            var match = videoIdRegex.Match(text);
+
+            Assert.AreEqual(succeed, match.Success);
+            Assert.AreEqual(2, match.Groups.Count);
+
+            if (succeed)
+            {
+                Assert.AreEqual(value, match.Value);
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow("http://www.nicovideo.jp/watch/sm22635959 , nm22635959", "sm22635959","nm22635959", true)]
+        public void MatchsTest(string text, string value1, string value2, bool succeed)
+        {
+            var videoIdRegex = createRegex();
+            var matchs = videoIdRegex.Matches(text);
+
+
+        }
+
+
+
+
+        private Regex createRegex()
+        {
+            return  new Regex(NiconicoTextPatterns.videoIdGroupPattern);
         }
     }
 }
