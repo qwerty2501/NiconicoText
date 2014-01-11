96249f900b3706fc0d7f84687c86390a10109617
クワーティ <riot313@gmail.com>
2014-01-11 14:00:42 +0900

    added line break tests

--- NiconicoText/NiconicoTextTest/Tests/LineBreakRegexTest.cs
+++ NiconicoText/NiconicoTextTest/Tests/LineBreakRegexTest.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using NiconicoText;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace NiconicoTextTest.Tests
 {
-    class LineBreakRegexTest
+    [TestClass]
+    public class LineBreakRegexTest
     {
+        [TestMethod]
+        public void CraeteTest()
+        {
+            var regex = createRegex();
+        }
+
+        private Regex createRegex()
+        {
+            return new Regex(NiconicoTextPatterns.lineBreakGroupPattern);
+        }
     }
 }
