cf373089d7ade3a9c984b2e13631bf96209c22dd
クワーティ <riot313@gmail.com>
2014-01-11 12:51:56 +0900

    added watch still image Id tests

--- NiconicoText/NiconicoTextTest/Tests/WatchStillImageIdRegexTest.cs
+++ NiconicoText/NiconicoTextTest/Tests/WatchStillImageIdRegexTest.cs
@@ -1,15 +1,26 @@
 ﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using NiconicoText;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace NiconicoTextTest.Tests
 {
     [TestClass]
-    class WatchStillImageIdRegexTest
+    public class WatchStillImageIdRegexTest
     {
-        
+        [TestMethod]
+        public void CreateTest()
+        {
+            var regex = createRegex();
+        }
+
+        private Regex createRegex()
+        {
+            return new Regex(NiconicoTextPatterns.watchStillImageIdGroupPattern);
+        }
     }
 }
