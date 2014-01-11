9095cba9a6f3a73a7d89ce693266db1d7aa071cf
クワーティ <riot313@gmail.com>
2014-01-11 13:00:12 +0900

    added StillImageIdRegex tests

--- NiconicoText/NiconicoTextTest/Tests/StillImageIdRegexTest.cs
+++ NiconicoText/NiconicoTextTest/Tests/StillImageIdRegexTest.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using NiconicoText;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace NiconicoTextTest.Tests
 {
-    class StillImageIdRegexTest
+    [TestClass]
+    public class StillImageIdRegexTest
     {
+        [TestMethod]
+        public void CreateTest()
+        {
+            var regex = creteRegex();
+        }
+
+        private Regex creteRegex()
+        {
+            return new Regex(NiconicoTextPatterns.stillImageIdGroupPattern);
+        }
     }
 }
