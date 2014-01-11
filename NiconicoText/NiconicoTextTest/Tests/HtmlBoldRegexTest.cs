b4a85104e72485a2bf73172c4f6cadc1c3a8be7b
クワーティ <riot313@gmail.com>
2014-01-11 14:14:26 +0900

    added HtmlBoldRegexTest

--- NiconicoText/NiconicoTextTest/Tests/HtmlBoldRegexTest.cs
+++ NiconicoText/NiconicoTextTest/Tests/HtmlBoldRegexTest.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using NiconicoText;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace NiconicoTextTest.Tests
 {
-    class HtmlBoldRegexTest
+    [TestClass]
+    public class HtmlBoldRegexTest
     {
+        [TestMethod]
+        public void CreateTest()
+        {
+
+        }
+
+        private Regex createRegex()
+        {
+            return new Regex(NiconicoTextPatterns.htmlBoldGroupPattern);
+        }
     }
 }
