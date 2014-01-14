ef3ac4de94135fcee060e70b7214d28e88ea1059
クワーティ <riot313@gmail.com>
2014-01-14 22:17:23 +0900

    deleted NiconicoBodyText

--- NiconicoText/NiconicoText/NiconicoTextPatterns.cs
+++ NiconicoText/NiconicoText/NiconicoTextPatterns.cs
@@ -111,13 +111,13 @@ namespace NiconicoText
 
 
 			
-		internal const string htmlFontPattern=@"<(?i:font)\s+?(?:(?:(?i:color)=\""(?<color>#?[a-fA-F0-9]+)|(?i:size)=\""(?<size>[+-]?[1-7]))\""\s*?)*?>(?<fontText>.*?)</(?i:font)\s*?>";
+		internal const string htmlFontPattern=@"<(?i:font)\s+?(?:(?:(?i:color)=\""(?:(?<colorCode>#?[a-fA-F0-9]{6})|(?<colorName>[a-zA-Z]{1,25}))|(?i:size)=\""(?<size>[+-]?[1-7]))\""\s*?)*?>(?<fontText>.*?)</(?i:font)\s*?>";
 
 
 		internal const string htmlFont="htmlFont";
 
 
-		internal const string htmlFontGroupPattern=@"(?<htmlFont><(?i:font)\s+?(?:(?:(?i:color)=\""(?<color>#?[a-fA-F0-9]+)|(?i:size)=\""(?<size>[+-]?[1-7]))\""\s*?)*?>(?<fontText>.*?)</(?i:font)\s*?>)";
+		internal const string htmlFontGroupPattern=@"(?<htmlFont><(?i:font)\s+?(?:(?:(?i:color)=\""(?:(?<colorCode>#?[a-fA-F0-9]{6})|(?<colorName>[a-zA-Z]{1,25}))|(?i:size)=\""(?<size>[+-]?[1-7]))\""\s*?)*?>(?<fontText>.*?)</(?i:font)\s*?>)";
 
 
 			
@@ -181,151 +181,107 @@ namespace NiconicoText
 
 
 			
-		internal const string niconicoTextParsePattern = @"(?:(?<communityId>co\d{1,14})|(?<channelId>ch\d{1,14})|(?<videoId>(?:sm|nm|so|ca|ax|yo|nl|ig|na|cw|z[a-e]|om|sk|yk)\d{1,14})|(?<articleId>ar\d{1,14})|(?<materialId>nc\d{1,14})|(?<marketItemId>(?:dw\d+|az[A-Z0-9]{10}|ys[a-zA-Z0-9-]+_[a-zA-Z0-9-]|ga\d+|ip[\d_]+|gg[a-zA-Z0-9]+-[a-zA-Z0-9-]+))|(?<liveId>lv\d{1,14})|(?<watchStillImageId>(?:[sm]g|im)\d{1,14})|(?<stillImageId>im\d{1,14})|(?<url>https?://[\w/:%#\$&\?\(\)~\.=\+\-]+)|(?<htmlFont><(?i:font)\s+?(?:(?:(?i:color)=\""(?<color>#?[a-fA-F0-9]+)|(?i:size)=\""(?<size>[+-]?[1-7]))\""\s*?)*?>(?<fontText>.*?)</(?i:font)\s*?>)|(?<lineBreak>(?:\r?\n|<(?i:br)\s*?/?>))|(?<htmlBold><[bB]\s*?>(?<boldText>.*?)</[bB]\s*?>)|(?<htmlItalic><[iI]\s*?>(?<italicText>.*?)</[iI]\s*?>)|(?<htmlStrike><[sS]\s*?>(?<strikeText>.*?)</[sS]\s*?>)|(?<htmlUnderLine><[uU]\s*?>(?<underLineText>.*?)</[uU]\s*?>)|(?<invalidHtmlElement></?.*?>))";
+		internal const string niconicoTextParsePattern = @"(?:(?<communityId>co\d{1,14})|(?<channelId>ch\d{1,14})|(?<videoId>(?:sm|nm|so|ca|ax|yo|nl|ig|na|cw|z[a-e]|om|sk|yk)\d{1,14})|(?<articleId>ar\d{1,14})|(?<materialId>nc\d{1,14})|(?<marketItemId>(?:dw\d+|az[A-Z0-9]{10}|ys[a-zA-Z0-9-]+_[a-zA-Z0-9-]|ga\d+|ip[\d_]+|gg[a-zA-Z0-9]+-[a-zA-Z0-9-]+))|(?<liveId>lv\d{1,14})|(?<watchStillImageId>(?:[sm]g|im)\d{1,14})|(?<stillImageId>im\d{1,14})|(?<url>https?://[\w/:%#\$&\?\(\)~\.=\+\-]+)|(?<htmlFont><(?i:font)\s+?(?:(?:(?i:color)=\""(?:(?<colorCode>#?[a-fA-F0-9]{6})|(?<colorName>[a-zA-Z]{1,25}))|(?i:size)=\""(?<size>[+-]?[1-7]))\""\s*?)*?>(?<fontText>.*?)</(?i:font)\s*?>)|(?<lineBreak>(?:\r?\n|<(?i:br)\s*?/?>))|(?<htmlBold><[bB]\s*?>(?<boldText>.*?)</[bB]\s*?>)|(?<htmlItalic><[iI]\s*?>(?<italicText>.*?)</[iI]\s*?>)|(?<htmlStrike><[sS]\s*?>(?<strikeText>.*?)</[sS]\s*?>)|(?<htmlUnderLine><[uU]\s*?>(?<underLineText>.*?)</[uU]\s*?>)|(?<invalidHtmlElement></?.*?>))";
 
 
 		internal const int communityIdGroupNumber=1;
 
 
-					
-
 				
 		internal const int channelIdGroupNumber=2;
 
-
-					
 
 				
 		internal const int videoIdGroupNumber=3;
 
 
-					
-
 				
 		internal const int articleIdGroupNumber=4;
 
-
-					
 
 				
 		internal const int materialIdGroupNumber=5;
 
 
-					
-
 				
 		internal const int marketItemIdGroupNumber=6;
 
-
-					
 
 				
 		internal const int liveIdGroupNumber=7;
 
 
-					
-
 				
 		internal const int watchStillImageIdGroupNumber=8;
 
-
-					
 
 				
 		internal const int stillImageIdGroupNumber=9;
 
 
-					
-
 				
 		internal const int urlGroupNumber=10;
 
-
-					
 
 				
 		internal const int htmlFontGroupNumber=11;
 
 
-					
+				
+		internal const int colorCodeGroupNumber=12;
+
 
 				
-		internal const int colorGroupNumber=12;
+		internal const int colorNameGroupNumber=13;
 
-
-					
 
 				
-		internal const int sizeGroupNumber=13;
+		internal const int sizeGroupNumber=14;
 
-
-					
 
 				
-		internal const int fontTextGroupNumber=14;
+		internal const int fontTextGroupNumber=15;
 
-
-					
 
 				
-		internal const int lineBreakGroupNumber=15;
+		internal const int lineBreakGroupNumber=16;
 
-
-					
 
 				
-		internal const int htmlBoldGroupNumber=16;
+		internal const int htmlBoldGroupNumber=17;
 
-
-					
 
 				
-		internal const int boldTextGroupNumber=17;
+		internal const int boldTextGroupNumber=18;
 
-
-					
 
 				
-		internal const int htmlItalicGroupNumber=18;
+		internal const int htmlItalicGroupNumber=19;
 
-
-					
 
 				
-		internal const int italicTextGroupNumber=19;
+		internal const int italicTextGroupNumber=20;
 
-
-					
 
 				
-		internal const int htmlStrikeGroupNumber=20;
+		internal const int htmlStrikeGroupNumber=21;
 
-
-					
 
 				
-		internal const int strikeTextGroupNumber=21;
+		internal const int strikeTextGroupNumber=22;
 
-
-					
 
 				
-		internal const int htmlUnderLineGroupNumber=22;
+		internal const int htmlUnderLineGroupNumber=23;
 
-
-					
 
 				
-		internal const int underLineTextGroupNumber=23;
+		internal const int underLineTextGroupNumber=24;
 
-
-					
 
 				
-		internal const int invalidHtmlElementGroupNumber=24;
+		internal const int invalidHtmlElementGroupNumber=25;
 
-
-					
 
 				    }
 
@@ -353,181 +309,127 @@ namespace NiconicoText
 		internal const string htmlAnchorGroupPattern=@"(?<htmlAnchor><[aA]\s+?.*?href=\""(?<href>https?://[\w/:%#\$&\?\(\)~\.=\+\-]+)\""\s*?.*?>(?<anchorText>.*?)</[aA]\s*?>)";
 
 
-					internal const string niconicoCommentTextParsePattern = @"(?:(?<numberAnchor>>>[0-9]+?(?:-(?<secondAnchor>[0-9]+?))?)|(?<htmlAnchor><[aA]\s+?.*?href=\""(?<href>https?://[\w/:%#\$&\?\(\)~\.=\+\-]+)\""\s*?.*?>(?<anchorText>.*?)</[aA]\s*?>))|(?:(?<communityId>co\d{1,14})|(?<channelId>ch\d{1,14})|(?<videoId>(?:sm|nm|so|ca|ax|yo|nl|ig|na|cw|z[a-e]|om|sk|yk)\d{1,14})|(?<articleId>ar\d{1,14})|(?<materialId>nc\d{1,14})|(?<marketItemId>(?:dw\d+|az[A-Z0-9]{10}|ys[a-zA-Z0-9-]+_[a-zA-Z0-9-]|ga\d+|ip[\d_]+|gg[a-zA-Z0-9]+-[a-zA-Z0-9-]+))|(?<liveId>lv\d{1,14})|(?<watchStillImageId>(?:[sm]g|im)\d{1,14})|(?<stillImageId>im\d{1,14})|(?<url>https?://[\w/:%#\$&\?\(\)~\.=\+\-]+)|(?<htmlFont><(?i:font)\s+?(?:(?:(?i:color)=\""(?<color>#?[a-fA-F0-9]+)|(?i:size)=\""(?<size>[+-]?[1-7]))\""\s*?)*?>(?<fontText>.*?)</(?i:font)\s*?>)|(?<lineBreak>(?:\r?\n|<(?i:br)\s*?/?>))|(?<htmlBold><[bB]\s*?>(?<boldText>.*?)</[bB]\s*?>)|(?<htmlItalic><[iI]\s*?>(?<italicText>.*?)</[iI]\s*?>)|(?<htmlStrike><[sS]\s*?>(?<strikeText>.*?)</[sS]\s*?>)|(?<htmlUnderLine><[uU]\s*?>(?<underLineText>.*?)</[uU]\s*?>)|(?<invalidHtmlElement></?.*?>))";
+					internal const string niconicoCommentTextParsePattern = @"(?:(?<numberAnchor>>>[0-9]+?(?:-(?<secondAnchor>[0-9]+?))?)|(?<htmlAnchor><[aA]\s+?.*?href=\""(?<href>https?://[\w/:%#\$&\?\(\)~\.=\+\-]+)\""\s*?.*?>(?<anchorText>.*?)</[aA]\s*?>))|(?:(?<communityId>co\d{1,14})|(?<channelId>ch\d{1,14})|(?<videoId>(?:sm|nm|so|ca|ax|yo|nl|ig|na|cw|z[a-e]|om|sk|yk)\d{1,14})|(?<articleId>ar\d{1,14})|(?<materialId>nc\d{1,14})|(?<marketItemId>(?:dw\d+|az[A-Z0-9]{10}|ys[a-zA-Z0-9-]+_[a-zA-Z0-9-]|ga\d+|ip[\d_]+|gg[a-zA-Z0-9]+-[a-zA-Z0-9-]+))|(?<liveId>lv\d{1,14})|(?<watchStillImageId>(?:[sm]g|im)\d{1,14})|(?<stillImageId>im\d{1,14})|(?<url>https?://[\w/:%#\$&\?\(\)~\.=\+\-]+)|(?<htmlFont><(?i:font)\s+?(?:(?:(?i:color)=\""(?:(?<colorCode>#?[a-fA-F0-9]{6})|(?<colorName>[a-zA-Z]{1,25}))|(?i:size)=\""(?<size>[+-]?[1-7]))\""\s*?)*?>(?<fontText>.*?)</(?i:font)\s*?>)|(?<lineBreak>(?:\r?\n|<(?i:br)\s*?/?>))|(?<htmlBold><[bB]\s*?>(?<boldText>.*?)</[bB]\s*?>)|(?<htmlItalic><[iI]\s*?>(?<italicText>.*?)</[iI]\s*?>)|(?<htmlStrike><[sS]\s*?>(?<strikeText>.*?)</[sS]\s*?>)|(?<htmlUnderLine><[uU]\s*?>(?<underLineText>.*?)</[uU]\s*?>)|(?<invalidHtmlElement></?.*?>))";
 
 
 		internal const int numberAnchorGroupNumber=1;
 
 
-					
-
 				
 		internal const int secondAnchorGroupNumber=2;
 
-
-					
 
 				
 		internal const int htmlAnchorGroupNumber=3;
 
 
-					
-
 				
 		internal const int hrefGroupNumber=4;
 
-
-					
 
 				
 		internal const int anchorTextGroupNumber=5;
 
 
-					
-
 				
 		internal const int communityIdGroupNumber=6;
 
-
-					
 
 				
 		internal const int channelIdGroupNumber=7;
 
 
-					
-
 				
 		internal const int videoIdGroupNumber=8;
 
-
-					
 
 				
 		internal const int articleIdGroupNumber=9;
 
 
-					
-
 				
 		internal const int materialIdGroupNumber=10;
 
-
-					
 
 				
 		internal const int marketItemIdGroupNumber=11;
 
 
-					
-
 				
 		internal const int liveIdGroupNumber=12;
 
-
-					
 
 				
 		internal const int watchStillImageIdGroupNumber=13;
 
 
-					
-
 				
 		internal const int stillImageIdGroupNumber=14;
 
-
-					
 
 				
 		internal const int urlGroupNumber=15;
 
 
-					
-
 				
 		internal const int htmlFontGroupNumber=16;
 
 
-					
+				
+		internal const int colorCodeGroupNumber=17;
+
 
 				
-		internal const int colorGroupNumber=17;
+		internal const int colorNameGroupNumber=18;
 
-
-					
 
 				
-		internal const int sizeGroupNumber=18;
+		internal const int sizeGroupNumber=19;
 
-
-					
 
 				
-		internal const int fontTextGroupNumber=19;
+		internal const int fontTextGroupNumber=20;
 
-
-					
 
 				
-		internal const int lineBreakGroupNumber=20;
+		internal const int lineBreakGroupNumber=21;
 
-
-					
 
 				
-		internal const int htmlBoldGroupNumber=21;
+		internal const int htmlBoldGroupNumber=22;
 
-
-					
 
 				
-		internal const int boldTextGroupNumber=22;
+		internal const int boldTextGroupNumber=23;
 
-
-					
 
 				
-		internal const int htmlItalicGroupNumber=23;
+		internal const int htmlItalicGroupNumber=24;
 
-
-					
 
 				
-		internal const int italicTextGroupNumber=24;
+		internal const int italicTextGroupNumber=25;
 
-
-					
 
 				
-		internal const int htmlStrikeGroupNumber=25;
+		internal const int htmlStrikeGroupNumber=26;
 
-
-					
 
 				
-		internal const int strikeTextGroupNumber=26;
+		internal const int strikeTextGroupNumber=27;
 
-
-					
 
 				
-		internal const int htmlUnderLineGroupNumber=27;
+		internal const int htmlUnderLineGroupNumber=28;
 
-
-					
 
 				
-		internal const int underLineTextGroupNumber=28;
+		internal const int underLineTextGroupNumber=29;
 
-
-					
 
 				
-		internal const int invalidHtmlElementGroupNumber=29;
+		internal const int invalidHtmlElementGroupNumber=30;
 
-
-					
 
 						}
 	}
