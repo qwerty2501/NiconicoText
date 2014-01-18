02491a08c4698b1df0a2ccb87743be4586ac6f39
クワーティ <riot313@gmail.com>
2014-01-18 17:22:22 +0900

    modified NiconicoWebTextSegmentCollection

--- NiconicoText/NiconicoText/NiconicoWebTextSegmentCollection.cs
+++ NiconicoText/NiconicoText/NiconicoWebTextSegmentCollection.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NiconicoText
 {
-    public class NiconicoWebTextSegmentCollection:List<INiconicoWebTextSegment> ,INiconicoWebTextSegmentCollection
+    internal class NiconicoWebTextSegmentCollection : Collection<INiconicoWebTextSegment>, INiconicoWebTextSegmentCollection
     {
     }
 }
