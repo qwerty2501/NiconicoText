1a3c69add8a5b479ebd5581d28e755e1d98f21ea
クワーティ <riot313@gmail.com>
2014-01-18 22:34:15 +0900

    created IdNiconicoWebTextSEgmentBase

--- NiconicoText/NiconicoText/PlainNiconicoWebTextSegmentWithUrlBase.cs
+++ NiconicoText/NiconicoText/PlainNiconicoWebTextSegmentWithUrlBase.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace NiconicoText
 {
-    class PlainNiconicoWebTextSegmentWithUrlBase
+    internal abstract class PlainNiconicoWebTextSegmentWithUrlBase:PlainNiconicoWebTextSegmentBase,INiconicoWebTextSegment
     {
+        internal PlainNiconicoWebTextSegmentWithUrlBase(string text):base(text)
+        {
+            this.url_ = new WeakReference<Uri>(null);
+        }
+
+        private WeakReference<Uri> url_;
+
+        public new Uri Url
+        {
+            get
+            {
+                Uri url;
+
+                if (this.url_.TryGetTarget(out url))
+                {
+                    return url;
+                }
+                else
+                {
+                    url = OnCreateUrl();
+                    this.url_.SetTarget(url);
+                    return url;
+                }
+            }
+        }
+
+        internal protected abstract Uri OnCreateUrl();
+
     }
 }
