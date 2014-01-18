f1f277f68c3f3fe79b01509b4dee8d7782dee167
クワーティ <riot313@gmail.com>
2014-01-18 22:54:05 +0900

    changed class name

--- NiconicoText/NiconicoText/IdNiconicoWebTextSEgmentBase.cs
+++ NiconicoText/NiconicoText/IdNiconicoWebTextSEgmentBase.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NiconicoText
 {
-    internal abstract class IdNiconicoWebTextSEgmentBase:PlainNiconicoWebTextSegmentWithUrlBase
+    internal abstract class IdNiconicoWebTextSegmentBase:PlainNiconicoWebTextSegmentWithUrlBase
     {
         protected internal override Uri OnCreateUrl()
         {
