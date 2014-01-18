80d5fcd8b4d43b75cd89087ce11f1e5c974855d0
クワーティ <riot313@gmail.com>
2014-01-18 17:46:08 +0900

    changed class name

---
+++ NiconicoText/NiconicoText/NiconicoWebTextSegmentBase.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiconicoText
+{
+    internal class NiconicoWebTextSegmentBase:INiconicoWebTextSegment
+    {
+
+
+
+        public bool HasUrl
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool HasChild
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public INiconicoWebTextSegmentCollection ChildSegments
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public byte FontSize
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool AssociatedUnderLine
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool AssociatedStrike
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool AssociatedItalic
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Uri Url
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public INiconicoWebTextSegment Parent
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Text
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string FriendlyText
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public NiconicoTextColor Color
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
