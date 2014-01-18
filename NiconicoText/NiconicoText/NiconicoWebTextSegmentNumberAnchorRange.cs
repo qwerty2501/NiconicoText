8f62fb5cb3fe455497139c7aaa97d1f54039c502
クワーティ <riot313@gmail.com>
2014-01-18 20:49:20 +0900

    created NiconicoWebTextSegmentNumberAnchorRange

--- NiconicoText/NiconicoText/NiconicoWebTextSegmentNumberAnchorRange.cs
+++ NiconicoText/NiconicoText/NiconicoWebTextSegmentNumberAnchorRange.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NiconicoText
 {
-    internal class NiconicoWebTextSegmentCollection<T>:List<T>
-        where T:INiconicoWebTextSegment
-        
+    class NiconicoWebTextSegmentNumberAnchorRange
     {
     }
 }
