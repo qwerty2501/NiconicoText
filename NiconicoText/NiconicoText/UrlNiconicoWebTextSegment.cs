a3a66073d1efef6543bb24fa755e067802b980d5
クワーティ <riot313@gmail.com>
2014-01-19 00:00:22 +0900

    implemented UrlNiconicoWebTextSegment

--- NiconicoText/NiconicoText/UrlNiconicoWebTextSegment.cs
+++ NiconicoText/NiconicoText/UrlNiconicoWebTextSegment.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace NiconicoText
 {
-    class UrlNiconicoWebTextSegment
+    internal sealed class UrlNiconicoWebTextSegment:PlainNiconicoWebTextSegmentWithUrlBase,INiconicoWebTextSegment
     {
+        internal UrlNiconicoWebTextSegment(string url) : base(url) { }
+
+        protected internal override Uri OnCreateUrl()
+        {
+            return new Uri(this.text_);
+        }
+
+        public override NiconicoWebTextSegmentType SegmentType
+        {
+            get { return NiconicoWebTextSegmentType.Url; }
+        }
     }
 }
