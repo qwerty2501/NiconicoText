270473f6d844ce4256cbfaf5b9d0dcbba0e19b7e
クワーティ <riot313@gmail.com>
2014-01-19 16:03:28 +0900

    implemented LineBreakNiconicoWebTextSegment

--- NiconicoText/NiconicoText/LineBreakNiconicoWebTextSegment.cs
+++ NiconicoText/NiconicoText/LineBreakNiconicoWebTextSegment.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace NiconicoText
 {
-    class LineBreakNiconicoWebTextSegment
+    internal abstract class LineBreakNiconicoWebTextSegmentBase:NiconicoWebTextSegmentBase,INiconicoWebTextSegment,INiconicoTextSegment
     {
+        public new string FriendlyText
+        {
+            get
+            {
+                return Environment.NewLine;
+            }
+        }
+
+        public override NiconicoWebTextSegmentType SegmentType
+        {
+            get { return NiconicoWebTextSegmentType.LineBreak; }
+        }
+    }
+
+    internal sealed class LineBreakNiconicoWebTextSegment : LineBreakNiconicoWebTextSegmentBase, INiconicoWebTextSegment, INiconicoTextSegment
+    {
+
+        public override string Text
+        {
+            get { return Environment.NewLine; }
+        }
+    }
+
+    internal sealed class HtmlBreakElementNiconicoWebTextSegment : LineBreakNiconicoWebTextSegmentBase, INiconicoWebTextSegment, INiconicoTextSegment
+    {
+
+        public override string Text
+        {
+            get { return "<br>"; }
+        }
     }
 }
