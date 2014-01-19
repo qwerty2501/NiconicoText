2ae65a20772bdc74b1c27b2075b08ba08f84e300
クワーティ <riot313@gmail.com>
2014-01-19 17:58:37 +0900

    implemented SegmentsProsessionNiconicoWebTextSegmentBase

--- NiconicoText/NiconicoText/SegmentsProsessionNiconicoWebTextSegmentBase.cs
+++ NiconicoText/NiconicoText/SegmentsProsessionNiconicoWebTextSegmentBase.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace NiconicoText
 {
-    class SegmentsProsessionNiconicoWebTextSegmentBase
+    internal abstract class SegmentsProsessionNiconicoWebTextSegmentBase:NiconicoWebTextSegmentBase,INiconicoWebTextSegment,INiconicoTextSegment
     {
+        internal SegmentsProsessionNiconicoWebTextSegmentBase(INiconicoWebTextSegmentCollection segments)
+        {
+            this.segments_ = segments;
+        }
+
+
+        public new INiconicoWebTextSegmentCollection Segments
+        {
+            get
+            {
+                return this.segments_;
+            }
+        }
+
+        public override string Text
+        {
+            get
+            {
+                return this.segments_.ToString();
+            }
+        }
+
+        public new string FriendlyText
+        {
+            get
+            {
+                return this.segments_.ToFriendlyString();
+            }
+        }
+
+        private INiconicoWebTextSegmentCollection segments_;
+
+
+
     }
 }
