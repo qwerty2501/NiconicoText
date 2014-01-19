dc357ca9bb595e3bddd8bb7ccb6ead5a5a5c4035
クワーティ <riot313@gmail.com>
2014-01-19 14:45:31 +0900

    created PictureIdNiconicoWebTextSegment

--- NiconicoText/NiconicoText/UrlNiconicoWebTextSegment.cs
+++ NiconicoText/NiconicoText/UrlNiconicoWebTextSegment.cs
@@ -6,18 +6,37 @@ using System.Threading.Tasks;
 
 namespace NiconicoText
 {
-    internal sealed class UrlNiconicoWebTextSegment:PlainNiconicoWebTextSegmentWithUrlBase,INiconicoWebTextSegment
+    internal sealed class UrlNiconicoWebTextSegment:PlainNiconicoWebTextSegmentBase,INiconicoWebTextSegment
     {
-        internal UrlNiconicoWebTextSegment(string url) : base(url) { }
+        internal UrlNiconicoWebTextSegment(string url) : base(url) 
+        { 
+            this.urlRef_ = new WeakReference<Uri>(null);
+        }
 
-        protected internal override Uri OnCreateUrl()
+        public new Uri Url
         {
-            return new Uri(this.text_);
+            get
+            {
+                Uri url;
+                if (this.urlRef_.TryGetTarget(out url))
+                {
+                    return url;
+                }
+                else
+                {
+                    url = new Uri(this.text_);
+                    this.urlRef_.SetTarget(url);
+                    return url;
+                }
+            }
         }
 
         public override NiconicoWebTextSegmentType SegmentType
         {
             get { return NiconicoWebTextSegmentType.Url; }
         }
+
+
+        private WeakReference<Uri> urlRef_;
     }
 }
