ead656a5cc88bbb58ec200adf0c0e6cc7aef9f04
クワーティ <riot313@gmail.com>
2014-01-19 18:45:06 +0900

    implemented HtmlItalicElementNiconicoWebTextSegment

---
+++ NiconicoText/NiconicoText/HtmlItalicElementNiconicoWebTextSegment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiconicoText
+{
+    internal sealed class HtmlItalicElementNiconicoWebTextSegment:SegmentsProsessionNiconicoWebTextSegmentBase,INiconicoWebTextSegment,INiconicoTextSegment
+    {
+        internal HtmlItalicElementNiconicoWebTextSegment(INiconicoWebTextSegmentCollection segments) : base(segments) { }
+
+        public new bool AssociatedItalic
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override NiconicoWebTextSegmentType SegmentType
+        {
+            get { return NiconicoWebTextSegmentType.HtmlItalicElement; }
+        }
+    }
+}
