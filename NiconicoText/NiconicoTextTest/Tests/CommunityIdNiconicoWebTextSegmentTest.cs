f49ec6c3ee5346d2fac4dea1564d56019ec0f901
クワーティ <riot313@gmail.com>
2014-01-22 23:07:27 +0900

    implemented CommunityIdNiconicoWebTextSegmentTest

--- NiconicoText/NiconicoTextTest/Tests/CommunityIdNiconicoWebTextSegmentTest.cs
+++ NiconicoText/NiconicoTextTest/Tests/CommunityIdNiconicoWebTextSegmentTest.cs
@@ -1,1 +1,56 @@
-﻿
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using NiconicoText;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace NiconicoTextTest.Tests
+{
+    [TestClass]
+    public class CommunityIdNiconicoWebTextSegmentTest
+    {
+        [TestMethod]
+        public void CreateTest()
+        {
+            CommunityIdNiconicoWebTextSegment val =  new CommunityIdNiconicoWebTextSegment("co55555555");
+
+            INiconicoWebTextSegment segment = val;
+
+            Assert.IsFalse( segment.DecoratedColor);
+            Assert.IsFalse(segment.DecoratedBold);
+            Assert.IsFalse(segment.DecoratedItalic);
+            Assert.IsFalse(segment.DecoratedStrike);
+            Assert.IsFalse(segment.DecoratedUnderLine);
+            Assert.IsFalse(segment.HasNumberAnchor);
+            Assert.IsFalse(segment.HasSegments);
+            Assert.IsFalse(segment.HasUrl);
+            Assert.AreEqual(new NiconicoTextColor { R = 0, G = 0, B = 0 }, segment.Color);
+            Assert.AreEqual(new NiconicoWebTextNumberAnchorRange { StartNumber = 0,EndNumber = 0}, segment.NumberAnchor);
+            Assert.AreEqual(null, segment.Parent);
+            Assert.AreEqual(null, segment.Segments);
+            Assert.AreEqual(null, segment.Url);
+            Assert.AreEqual("co55555555", segment.Text);
+            Assert.AreEqual(NiconicoWebTextSegmentType.CommunityId, segment.SegmentType);
+            Assert.AreEqual(3, segment.FontElementSize);
+            Assert.AreEqual("co55555555", segment.FriendlyText);
+        }
+
+        [DataTestMethod]
+        [DataRow("co55555555")]
+        public void ParseWebTextTest(string text)
+        {
+            var regex = new Regex(NiconicoWebTextPatterns.niconicoWebTextParsePattern);
+            var segmenter = new NiconicoWebTextSegmenter();
+            var match = regex.Match(text);
+            Assert.IsTrue(match.Success);
+            INiconicoWebTextSegment segment = CommunityIdNiconicoWebTextSegment.ParseWebText(match, segmenter);
+            Assert.AreEqual(NiconicoWebTextSegmentType.CommunityId, segment.SegmentType);
+        }
+
+
+    }
+}
