b04ca70571af16c6578e58d44ab5f67fdfffb625
クワーティ <riot313@gmail.com>
2014-01-22 20:17:57 +0900

    implemented WatchPictureIdNiconicoWebTextSegmentTest

--- NiconicoText/NiconicoTextTest/Tests/WatchPictureIdNiconicoWebTextSegmentTest.cs
+++ NiconicoText/NiconicoTextTest/Tests/WatchPictureIdNiconicoWebTextSegmentTest.cs
@@ -1,1 +1,55 @@
-﻿
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using NiconicoText;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace NiconicoTextTest.Tests
+{
+    [TestClass]
+    public class WatchPictureIdNiconicoWebTextSegmentTest
+    {
+        [TestMethod]
+        public void CreateTest()
+        {
+            WatchPictureIdNiconicoWebTextSegment val =  new WatchPictureIdNiconicoWebTextSegment("im222222222");
+
+            INiconicoWebTextSegment segment = val;
+
+            Assert.IsFalse( segment.DecoratedColor);
+            Assert.IsFalse(segment.DecoratedBold);
+            Assert.IsFalse(segment.DecoratedItalic);
+            Assert.IsFalse(segment.DecoratedStrike);
+            Assert.IsFalse(segment.DecoratedUnderLine);
+            Assert.IsFalse(segment.HasNumberAnchor);
+            Assert.IsFalse(segment.HasSegments);
+            Assert.IsFalse(segment.HasUrl);
+            Assert.AreEqual(new NiconicoWebTextNumberAnchorRange { StartNumber = 0,EndNumber = 0}, segment.NumberAnchor);
+            Assert.AreEqual(null, segment.Parent);
+            Assert.AreEqual(null, segment.Segments);
+            Assert.AreEqual(null, segment.Url);
+            Assert.AreEqual("im222222222", segment.Text);
+            Assert.AreEqual(NiconicoWebTextSegmentType.PictureId, segment.SegmentType);
+            Assert.AreEqual(3, segment.FontElementSize);
+            Assert.AreEqual("im222222222", segment.FriendlyText);
+        }
+
+        [DataTestMethod]
+        [DataRow("im222222222")]
+        public void ParseWebTextTest(string text)
+        {
+            var regex = new Regex(NiconicoWebTextPatterns.niconicoWebTextParsePattern);
+            var segmenter = new NiconicoWebTextSegmenter();
+            var match = regex.Match(text);
+            Assert.IsTrue(match.Success);
+            INiconicoWebTextSegment segment = WatchPictureIdNiconicoWebTextSegment.ParseWebText(match, segmenter);
+            Assert.AreEqual(NiconicoWebTextSegmentType.PictureId, segment.SegmentType);
+        }
+
+
+    }
+}
