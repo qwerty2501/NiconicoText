8e7933d1c72ceba726d1d5f2b37e7e470491d109
クワーティ <riot313@gmail.com>
2014-01-22 22:15:34 +0900

    implmeneted HtmlUnderLineNiconicoWebTextSegmentTest

--- NiconicoText/NiconicoTextTest/Tests/HtmlUnderLineNiconicoWebTextSegmentTest.cs
+++ NiconicoText/NiconicoTextTest/Tests/HtmlUnderLineNiconicoWebTextSegmentTest.cs
@@ -1,1 +1,58 @@
-﻿
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using NiconicoText;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace NiconicoTextTest.Tests
+{
+    [TestClass]
+    public class HtmlUnderLineNiconicoWebTextSegmentTest
+    {
+        [TestMethod]
+        public void CreateTest()
+        {
+            var segmentes = new NiconicoWebTextSegmentCollection();
+            segmentes.Add(new PlainNiconicoWebTextSegment("testText"));
+            HtmlUnderLineNiconicoWebTextSegment val =  new HtmlUnderLineNiconicoWebTextSegment(segmentes);
+
+            INiconicoWebTextSegment segment = val;
+
+            Assert.IsFalse( segment.DecoratedColor);
+            Assert.IsFalse(segment.DecoratedBold);
+            Assert.IsFalse(segment.DecoratedItalic);
+            Assert.IsFalse(segment.DecoratedStrike);
+            Assert.IsTrue(segment.DecoratedUnderLine);
+            Assert.IsFalse(segment.HasNumberAnchor);
+            Assert.IsTrue(segment.HasSegments);
+            Assert.IsFalse(segment.HasUrl);
+            Assert.AreEqual(new NiconicoTextColor { R = 0, G = 0, B = 0 }, segment.Color);
+            Assert.AreEqual(new NiconicoWebTextNumberAnchorRange { StartNumber = 0,EndNumber = 0}, segment.NumberAnchor);
+            Assert.AreEqual(null, segment.Parent);
+            Assert.AreEqual(segmentes, segment.Segments);
+            Assert.AreEqual(null, segment.Url);
+            Assert.AreEqual("<u>testText</u>", segment.Text);
+            Assert.AreEqual(NiconicoWebTextSegmentType.HtmlUnderLineElement, segment.SegmentType);
+            Assert.AreEqual(3, segment.FontElementSize);
+            Assert.AreEqual("testText", segment.FriendlyText);
+        }
+
+        [DataTestMethod]
+        [DataRow("<u>testText</u>")]
+        public void ParseWebTextTest(string text)
+        {
+            var regex = new Regex(NiconicoWebTextPatterns.niconicoWebTextParsePattern);
+            var segmenter = new NiconicoWebTextSegmenter();
+            var match = regex.Match(text);
+            Assert.IsTrue(match.Success);
+            INiconicoWebTextSegment segment = HtmlUnderLineNiconicoWebTextSegment.ParseWebText(match, segmenter);
+            Assert.AreEqual(NiconicoWebTextSegmentType.HtmlUnderLineElement, segment.SegmentType);
+        }
+
+
+    }
+}
