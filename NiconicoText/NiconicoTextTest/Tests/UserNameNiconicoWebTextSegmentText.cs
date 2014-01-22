557551e121f3b3baf70d94592e49309d25dcdd41
クワーティ <riot313@gmail.com>
2014-01-22 20:38:48 +0900

    implemented UserNameNiconicoWebTextSegmentTest

--- NiconicoText/NiconicoTextTest/Tests/UserNameNiconicoWebTextSegmentText.cs
+++ NiconicoText/NiconicoTextTest/Tests/UserNameNiconicoWebTextSegmentText.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using NiconicoText;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace NiconicoTextTest.Tests
+{
+    [TestClass]
+    public class UserNameNiconicoWebTextSegmentTest
+    {
+        [TestMethod]
+        public void CreateTest()
+        {
+            UserNameNiconicoWebTextSegment val = new UserNameNiconicoWebTextSegment("userName");
+
+            INiconicoWebTextSegment segment = val;
+
+            Assert.IsFalse( segment.DecoratedColor);
+            Assert.IsFalse(segment.DecoratedBold);
+            Assert.IsFalse(segment.DecoratedItalic);
+            Assert.IsFalse(segment.DecoratedStrike);
+            Assert.IsFalse(segment.DecoratedUnderLine);
+            Assert.IsFalse(segment.HasNumberAnchor);
+            Assert.IsFalse(segment.HasSegments);
+            Assert.IsFalse(segment.HasUrl);
+            Assert.AreEqual(new NiconicoWebTextNumberAnchorRange { StartNumber = 0,EndNumber = 0}, segment.NumberAnchor);
+            Assert.AreEqual(null, segment.Parent);
+            Assert.AreEqual(null, segment.Segments);
+            Assert.AreEqual(null, segment.Url);
+            Assert.AreEqual("userName", segment.Text);
+            Assert.AreEqual(NiconicoWebTextSegmentType.UserName, segment.SegmentType);
+            Assert.AreEqual(3, segment.FontElementSize);
+            Assert.AreEqual("userName", segment.FriendlyText);
+        }
+
+
+    }
+}
