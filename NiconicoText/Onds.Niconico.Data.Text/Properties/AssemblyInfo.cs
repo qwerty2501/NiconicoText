0541c2ec28ef1ed2dca2bfa108dea051c029b7e1
クワーティ <riot313@gmail.com>
2014-01-29 20:20:53 +0900

    modified AssemblyInfo

--- NiconicoText/Onds.Niconico.Data.Text/Properties/AssemblyInfo.cs
+++ NiconicoText/Onds.Niconico.Data.Text/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Onds.Niconico.Data.Text")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2014 Onds")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
