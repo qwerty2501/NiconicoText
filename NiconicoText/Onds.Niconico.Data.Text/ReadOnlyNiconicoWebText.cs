7beac44af41c33df9cabd11d83541be185fc7825
クワーティ <riot313@gmail.com>
2014-02-02 20:59:37 +0900

    implemented ReadOnlyNiconicoWebText

--- NiconicoText/Onds.Niconico.Data.Text/ReadOnlyNiconicoWebText.cs
+++ NiconicoText/Onds.Niconico.Data.Text/ReadOnlyNiconicoWebText.cs
@@ -6,7 +6,105 @@ using System.Threading.Tasks;
 
 namespace Onds.Niconico.Data.Text
 {
-    class ReadOnlyNiconicoWebText
+
+    using Color = NiconicoTextColor;
+
+    internal class ReadOnlyNiconicoWebText:IReadOnlyNiconicoWebText,IReadOnlyNiconicoWebTextSegment,INiconicoText,INiconicoTextSegment
     {
+        public NiconicoTextType TextType
+        {
+            get { return NiconicoTextType.Web; }
+        }
+
+        public bool HasUrl
+        {
+            get { return false; }
+        }
+
+        public bool HasSegments
+        {
+            get { return true; }
+        }
+
+        public NiconicoWebTextSegmentType SegmentType
+        {
+            get { return NiconicoWebTextSegmentType.FullText; }
+        }
+
+        public IReadOnlyList<IReadOnlyNiconicoWebTextSegment> Segments
+        {
+            get;
+            internal set;
+        }
+
+        public bool HasNumberAnchor
+        {
+            get { return false; }
+        }
+
+        public NiconicoWebTextNumberAnchorRange NumberAnchor
+        {
+            get { return default(NiconicoWebTextNumberAnchorRange); }
+        }
+
+        public byte FontElementSize
+        {
+            get { return Onds.Niconico.Data.Text.FontElementSize.defaultSize; }
+        }
+
+        public bool DecoratedUnderLine
+        {
+            get { return false; }
+        }
+
+        public bool DecoratedBold
+        {
+            get { return false; }
+        }
+
+        public bool DecoratedStrike
+        {
+            get { return false; }
+        }
+
+        public bool DecoratedItalic
+        {
+            get { return false; }
+        }
+
+        public bool DecoratedColor
+        {
+            get { return false; }
+        }
+
+        public Color Color
+        {
+            get { return default(Color); }
+        }
+
+        public Uri Url
+        {
+            get { return null; }
+        }
+
+        public IReadOnlyNiconicoWebTextSegment Parent
+        {
+            get { return null; }
+        }
+
+        public IReadOnlyNiconicoWebText Root
+        {
+            get { return this; }
+        }
+
+        public string Text
+        {
+            get { return this.Segments.ToText(); }
+        }
+
+        public string FriendlyText
+        {
+            get { return this.Segments.ToFriendlyText(); }
+        }
     }
 }
